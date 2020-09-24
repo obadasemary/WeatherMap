06fd96e3e210f46dc9468cbfdd0a04a0a0828446
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-24 21:27:11 +0300

    Create WeatherResponse.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

---
+++ WeatherMap/Models/WeatherResponse.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherResponse.swift
+//  WeatherMap
+//
+//  Created by Abdelrahman Mohamed on 24.09.2020.
+//
+
+import Foundation
+
+struct WeatherResponse: Decodable {
+    
+    let main: Weather
+}
