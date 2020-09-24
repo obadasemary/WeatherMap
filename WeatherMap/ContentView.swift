643701038711db27ba91c469e5c76b08bf929c19
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-09-24 19:56:40 +0300

    Initial Commit

---
+++ WeatherMap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherMap
+//
+//  Created by Abdelrahman Mohamed on 24.09.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherMap/WeatherMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherMapApp.swift
+//  WeatherMap
+//
+//  Created by Abdelrahman Mohamed on 24.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct WeatherMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
